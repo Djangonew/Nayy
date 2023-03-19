aba2a309b7d97d634fe05ca6bae99dc9d2190f93
ayrizz <118755946+ayrizz@users.noreply.github.com>
2023-03-19 15:58:46 +0700

    update permitdb.py

--- Ubot/core/db/permitdb.py
+++ Ubot/core/db/permitdb.py
@@ -11,40 +11,57 @@ BLOCKED = "**Blokir !**"
 LIMIT = 5
 
 
-async def set_pm(value: bool):
-    doc = {"_id": 1, "pmpermit": value}
+async def set_pm(user_id: int, value: bool):
+    doc = {"_id": user_id, "pmpermit": value}
     doc2 = {"_id": "Approved", "users": []}
-    r = await collection.find_one({"_id": 1})
+    r = await collection.find_one({"_id": user_id})
     r2 = await collection.find_one({"_id": "Approved"})
     if r:
-        await collection.update_one({"_id": 1}, {"$set": {"pmpermit": value}})
+        await collection.update_one({"_id": user_id}, {"$set": {"pmpermit": value}})
     else:
         await collection.insert_one(doc)
     if not r2:
         await collection.insert_one(doc2)
 
 
-async def set_permit_message(text):
-    await collection.update_one({"_id": 1}, {"$set": {"pmpermit_message": text}})
+async def set_permit_message(user_id: int, text):
+    doc = {"_id": user_id, "pmpermit_message": text}
+    r = await collection.find_one({"_id": user_id})
+    if r:
+        await collection.update_one({"_id": user_id}, {"$set": {"pmpermit_message": text}})
+    else:
+        await collection.insert_one(doc)
 
 
-async def set_block_message(text):
-    await collection.update_one({"_id": 1}, {"$set": {"block_message": text}})
+async def set_block_message(user_id: int, text):
+    doc = {"_id": user_id, "block_message": text}
+    r = await collection.find_one({"_id": user_id})
+    if r:
+        await collection.update_one({"_id": user_id}, {"$set": {"block_message": text}})
+    else:
+        await collection.insert_one(doc)
 
 
-async def set_limit(limit):
-    await collection.update_one({"_id": 1}, {"$set": {"limit": limit}})
+async def set_limit(user_id: int, limit):
+    doc = {"_id": user_id, "limit": limit}
+    r = await collection.find_one({"_id": user_id})
+    if r:
+        await collection.update_one({"_id": user_id}, {"$set": {"limit": limit}})
+    else:
+        await collection.insert_one(doc)
+})
 
 
 async def get_pm_settings():
-    result = await collection.find_one({"_id": 1})
+    result = await collection.find_one({"_id": user_id})
     if not result:
         return False
     pmpermit = result["pmpermit"]
-    pm_message = result.get("pmpermit_message", PMPERMIT_MESSAGE)
-    block_message = result.get("block_message", BLOCKED)
+    pm_message = result.get("pmpermit_message", PM_PERMIT_MESSAGE)
+    block_message = result.get("block_message", BLOCKED_MESSAGE)
     limit = result.get("limit", LIMIT)
     return pmpermit, pm_message, limit, block_message
+
 
 
 async def allow_user(chat):
@@ -69,7 +86,7 @@ async def deny_user(chat):
 
 
 async def pm_guard():
-    result = await collection.find_one({"_id": 1})
+    result = await collection.find_one({"_id": user_id})
     if not result:
         return False
     if not result["pmpermit"]:
