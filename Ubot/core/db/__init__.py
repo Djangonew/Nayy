498319e4a5ef7b278b97a03e3e8926e1ef8ea53d
ayrizz <118755946+ayrizz@users.noreply.github.com>
2023-03-19 15:50:55 +0700

    update __init__.py

--- Ubot/core/db/__init__.py
+++ Ubot/core/db/__init__.py
@@ -1,20 +1,11 @@
 
 from ubotlibs.ubot.database import cli
+from config import MONGO_URL
 from .usersdb import *
 from .accesdb import *
 from .notesdb import *
 
-import logging
-import motor.motor_asyncio
-import codecs
-import pickle
+from motor.motor_asyncio import AsyncIOMotorClient
 
-from string import ascii_lowercase
-from typing import Dict, List, Union
-
-from config import MONGO_URL
-
-
-mongo_client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
-
-db_x = mongo_client["Naya"]
+client = AsyncIOMotorClient(MONGO_URL)
+db = client["Naya"]
