e8536f77535a1c1509fb08ace1da9806cff39be2
ayrizz <118755946+ayrizz@users.noreply.github.com>
2023-03-19 15:39:57 +0700

    update permitdb.py

--- Ubot/core/db/permitdb.py
+++ Ubot/core/db/permitdb.py
@@ -1,14 +1,4 @@
-import logging
-import motor.motor_asyncio
-import codecs
-import pickle
-
-from string import ascii_lowercase
-from typing import Dict, List, Union
-
-from config import MONGO_URL
-
-cli = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
+from . import cli
 
 collection = cli["pmpermit"]
 
