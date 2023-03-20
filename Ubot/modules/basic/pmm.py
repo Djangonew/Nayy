ca42742f55cc038f2927735e502d3c227c425379
ayrizz <118755946+ayrizz@users.noreply.github.com>
2023-03-21 00:18:30 +0700

    update pmm.py

--- Ubot/modules/basic/pmm.py
+++ Ubot/modules/basic/pmm.py
@@ -1,85 +1,75 @@
 
-import asyncio
-from pyrogram.methods import messages
 from pyrogram import filters, Client
 from pyrogram.types import Message
-from Ubot.core.db import permitdb as nay
 from Ubot.core.db.permitdb import *
-
 from . import *
 from config import BOTLOG_CHATID, PM_LOGGER
+from ubotlibs.ubot.utils import *
 
-
-FLOOD_CTRL = 0
-ALLOWED = []
-USERS_AND_WARNS = {}
-
-def get_arg(message):
-    msg = message.text
-    msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
-    split = msg[1:].replace("\n", " \n").split(" ")
-    if " ".join(split[1:]).strip() == "":
-        return ""
-    return " ".join(split[1:])
 
 
 async def denied_users(filter, client: Client, message: Message):
+    chat_id = message.chat.id
     user_id = message.from_user.id
-    if not await pm_guard(user_id):
+    if not await pm_guard(chat_id, user_id):
         return False
     if message.chat.id in (await get_approved_users()):
         return False
     elif message.from_user.id == DEVS:
+
         return False
+
     else:
         return True
 
-    
-@Client.on_message(filters.command(["setlimit"], cmds) & filters.me)
-async def pmguard(client, message):
-    arg = get_arg(message)
+
+@Ubot("setlimit", cmds)
+async def set_limit_cmd(client, message):
     user_id = message.from_user.id
+    arg = get_arg(message)
     if not arg:
-        await message.edit("**Gunakan format angka**")
+        await message.edit("`Gunakan format !setlimit angka`")
         return
-    await nay.set_limit(user_id, int(arg))
-    await message.edit(f"**Limit diset {arg}**")
-
-
-@Client.on_message(filters.command(["setblock"], cmds) & filters.me)
-async def setpmmsg(client, message):
-    arg = get_arg(message)
+    await set_limit(user_id, int(arg))
+    await message.edit(f"**Limit disetel ke {arg}**")
+    
+@Ubot("setblock", cmds)
+async def set_blocking_cmd(client, message):
     user_id = message.from_user.id
+    arg = get_arg(message)
     if not arg:
-        await message.edit("**Berikan pesan blokir**")
+        await message.edit("`Gunakan format !setblocking kata-kata`")
         return
     if arg == "default":
-        await nay.set_block_message(user_id, nay.BLOCKED)
-        await message.edit("**Block pesan diset default**.")
+        await set_block_message(user_id, BLOCKED)
+        await message.edit("**Pesan blokir diubah ke default**.")
         return
-    await nay.set_block_message(user_id, f"`{arg}`")
-    await message.edit("**Pesan Blokir Berhasil Diset**")
+    await set_block_message(user_id, f"`{arg}`")
+    await message.edit("**Pesan blokir disetel.**")
 
 
-@Client.on_message(filters.command(["a", "ok"], cmds) & filters.me & filters.private)
+@Client.on_message(filters.command(["y", "ok", "a", "iya"], cmds) & filters.me & filters.private)
 async def allow(client, message):
     chat_id = message.chat.id
     user_id = message.from_user.id
     pmpermit, pm_message, limit, block_message = await get_pm_settings(user_id)
-    await allow_user(chat_id)
+    await allow_user(chat_id, user_id)
     await message.edit(f"**Menerima pesan dari [you](tg://user?id={chat_id}).**")
     async for message in client.search_messages(
         chat_id=message.chat.id, query=pm_message, limit=1, from_user="me"
     ):
         await message.delete()
-    USERS_AND_WARNS.update({chat_id: 0})
+    USERS_AND_WARNS[chat_id] = 0
 
 
-@Client.on_message(filters.command(["no", "tolak"], cmds) & filters.me & filters.private)
+@Client.on_message(filters.command(["no", "n", "tolak", "g"], cmds) & filters.me & filters.private)
 async def deny(client, message):
     chat_id = message.chat.id
-    await deny_user(chat_id)
-    await message.edit(f"** [you](tg://user?id={chat_id}) DiTolak.**")
+    user_id = message.from_user.id
+    await deny_user(chat_id, user_id)
+    await message.edit(f"**I have denied [you](tg://user?id={chat_id}) to PM me.**")
+
+
 
 @Client.on_message(
     filters.private
@@ -90,25 +80,24 @@ async def deny(client, message):
     & ~filters.bot
     & ~filters.via_bot
 )
-async def reply_pm(client, message):
-    user_id = message.from_user.id
+async def reply_pm(client: Client, message):
     global FLOOD_CTRL
+    user_id = message.from_user.id
     pmpermit, pm_message, limit, block_message = await get_pm_settings(user_id)
-    user = message.from_user.id
-    user_warns = 0 if user not in USERS_AND_WARNS else USERS_AND_WARNS[user]
+    user_warns = 0 if user_id not in USERS_AND_WARNS else USERS_AND_WARNS[user_id]
     tai = f"<b>📨 PESAN BARU</b>\n<b> • : </b>{message.from_user.mention}"
     tai += f"\n<b> • 👀 </b><a href='{message.link}'>Lihat Pesan</a>"
     tai += f"\n<b> • Message : </b><code>{message.text}</code>"
-#    await asyncio.sleep(0.1)
-#    if PM_LOGGER:
-#        await app.send_message(
-#                 BOTLOG_CHATID,
-#                 tai,
-#                 parse_mode=enums.ParseMode.HTML,
-#                 disable_web_page_preview=True)
-    if user_warns <= limit - 2:
+    await asyncio.sleep(0.1)
+    if PM_LOGGER:
+        await app.send_message(
+                 BOTLOG_CHATID,
+                 tai,
+                 parse_mode=enums.ParseMode.HTML,
+                 disable_web_page_preview=True)
+    if pmpermit and user_warns <= limit - 2:
         user_warns += 1
-        USERS_AND_WARNS.update({user: user_warns})
+        USERS_AND_WARNS.update({user_id: user_warns})
         if not FLOOD_CTRL > 0:
             FLOOD_CTRL += 1
         else:
@@ -122,4 +111,4 @@ async def reply_pm(client, message):
         return
     await message.reply(block_message, disable_web_page_preview=True)
     await client.block_user(message.chat.id)
-    USERS_AND_WARNS.update({user: 0})
+    USERS_AND_WARNS.update({user_id: 0})
