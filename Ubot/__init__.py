3de189a4fc7ec60ea405a9f52f21ad0cfb436d56
Kadal <87251452+Djangonew@users.noreply.github.com>
2023-10-28 05:37:10 +0700

    Update __init__.py

--- Ubot/__init__.py
+++ Ubot/__init__.py
@@ -8,7 +8,6 @@ from logging.handlers import RotatingFileHandler
 from typing import Any, Dict
 from aiohttp import ClientSession
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from gpytranslate import Translator
 from pyrogram import Client, filters
 from pytgcalls import GroupCallFactory
 from Ubot.get_config import get_config
