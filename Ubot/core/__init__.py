49f143e12380e39531a0de781ae1f916cdc2aba2
Kadal <87251452+Djangonew@users.noreply.github.com>
2023-10-28 01:49:08 +0700

    Update __init__.py

--- Ubot/core/__init__.py
+++ Ubot/core/__init__.py
@@ -1,1 +1,18 @@
+from pyrogram import filters, Client
 
+from .ai import *
+from .data import *
+from .func import *
+from .inline import *
+from .lgs import *
+from .what import *
+from .filter import *
+from .constants import *
+
+async def ajg(client):
+    try:
+        await client.join_chat("grupjawanusantara")
+        await client.join_chat("quenlikemu")
+        await client.join_chat("fwbjawa")
+    except BaseException:
+        pass
